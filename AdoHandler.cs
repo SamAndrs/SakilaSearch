b2d99dc848dd98ffaf4f04d51281991b15044d68
SamAndrs <sam_andrs@yahoo.se>
2024-11-28 10:59:05 +0100

    update adohandler

--- AdoHandler.cs
+++ AdoHandler.cs
@@ -72,15 +72,7 @@ namespace ActorMovie_lookup
             ORDER BY actor.last_name
             THEN BY actor.first_name
             ;";
-            //THEN BY DESCENDING actor.first_name;";
-            /*
-            string query = @"
-            SELECT a.first_name, a.last_name
-            FROM actor a
-            JOIN film_actor fa ON a.actor_id = fa.actor_id
-            JOIN film f ON fa.film_id = f.film_id
-            WHERE f.title LIKE @name;";
-            */
+           
 
             DataTable actors = new DataTable();
 
